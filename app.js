ce7b6325a44df239888c009aeb7b3e61930513a7
Shtever <Shtevetm@gmail.com>
2020-09-24 19:24:39 -0500

    implemented bootstrap. added styling.

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ var x = "";
 
 // Generate Random Number 
 function random() {
-    x = (Math.floor(Math.random() * 1000) + 1);
-    console.log(x);
-    check();
-}
+//     x = (Math.floor(Math.random() * 1000) + 1);
+//     console.log(x);
+//     check();
+// }
 
-// Check if random number=goal number
-function check() {
-    if (x !== goal) {
-        x = (Math.floor(Math.random() * 1000) + 1);
-        count++;
-        random()
-    }
-    else {
-        console.log("Ya made it to " + goal + " in " + count + " tries")
-        alert("Ya made it to " + goal + " in " + count + " tries")
-    }
+// // Check if random number=goal number
+// function check() {
+//     if (x !== goal) {
+//         x = (Math.floor(Math.random() * 1000) + 1);
+//         count++;
+//         random()
+//     }
+//     else {
+//         console.log("Ya made it to " + goal + " in " + count + " tries")
+//         alert("Ya made it to " + goal + " in " + count + " tries")
+//     }
 } 

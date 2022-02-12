2a20b07fd84c098efcba67be7bede4cebcd74be8
Shtever <Shtevetm@gmail.com>
2022-02-12 00:01:55 -0600

    Added input field for search terms. Text value in search box will populate the API url. Need to refine search results.

--- untapped.js
+++ untapped.js
@@ -1,16 +1,28 @@
 // UNTAPPED API Functions
-var clientID="&client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42"
+var clientID="client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42"
 
 
 
-$(document).ready(function () {
-    var queryURL = "https://api.untappd.com/v4/beer/info/16630?" + clientID
+function submit(){
+    console.clear()
+    var beerName= document.getElementById('beerName').value;
+    console.log(beerName);
+    beerSearch();
+}
+
+
+function beerSearch() {
+    // Beer by ID
+    // var queryURL = "https://api.untappd.com/v4/beer/info/16630?" + "&" + clientID
+
+    // Beer by search term
+    var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerName.value + "&limit=50&" + clientID
 
     // Ajax call for lat/long based on user IP address //
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        console.log(response.response)
     })
-})
+}

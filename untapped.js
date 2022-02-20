de7af8d61462c90c17f11e4ce242274766fdfc36
Stephen McMillen <Shtevetm@gmail.com>
2022-02-19 19:53:42 -0600

    Revised API call. User info on page load, by search term on button submit.

--- untapped.js
+++ untapped.js
@@ -1,41 +1,40 @@
 // UNTAPPED API Functions
-var clientID="client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42"
+var clientID = "client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42"
+
+
+$(document).ready(userInfo())
 
 
 
-function submit(){
+
+function submit() {
     console.clear()
-    var beerName= document.getElementById('beerName').value;
+    var beerName = document.getElementById('beerName').value;
     console.log(beerName);
     beerSearch();
 }
 
 
 function beerSearch() {
-
-
     // BEER BY ID //
     // var queryURL = "https://api.untappd.com/v4/beer/info/16630?" + "&" + clientID //
 
-
-
     // BEER BY SEARCH TERM //
-    // var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerName.value + "&limit=50&" + clientID //
+    var queryURL = "https://api.untappd.com/v4/search/beer?q=" + beerName.value + "&limit=50&sort=beer_name&" + clientID
 
-    // Ajax call to Untappd //
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     console.log(response.response)
-    // })
+    // Ajax call to Untappd
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.response)
+    })
+}
 
-
-
-
-    // USER INFO //
-    var userName = "Quille"
-    var queryURL = "https://api.untappd.com/v4/user/info/" + userName + "?" + clientID 
+// USER INFO //
+function userInfo() {
+    var userName = "Mr_JFlow"
+    var queryURL = "https://api.untappd.com/v4/user/info/" + userName + "?" + clientID
 
     // Ajax call to Untappd //
     $.ajax({
@@ -45,5 +44,3 @@ function beerSearch() {
         console.log(response.response)
     })
 }
-
-

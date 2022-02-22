2aa4d9943c598f983d479a938e3f26dcf03a2784
Stephen McMillen <Shtevetm@gmail.com>
2022-02-22 16:30:43 -0600

    revised API function to return mutliple Untappd user info, from an array of usernames.

--- untapped.js
+++ untapped.js
@@ -1,10 +1,10 @@
 // UNTAPPED API Functions
-var clientID = "client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42"
+var clientID = "client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42";
 
 
-$(document).ready(userInfo())
+// $(document).ready(userInfo())
 
-
+document.onload = userInfo();
 
 
 function submit() {
@@ -33,14 +33,23 @@ function beerSearch() {
 
 // USER INFO //
 function userInfo() {
-    var userName = "Mr_JFlow"
-    var queryURL = "https://api.untappd.com/v4/user/info/" + userName + "?" + clientID
+    //Usernames//
+    var userName = ["Mr_JFlow", "Quille","Shtevetm"]
+    for (let i = 0; i<userName.length; i++){
+    var queryURL = "https://api.untappd.com/v4/user/info/" + userName[i] + "?" + clientID
 
     // Ajax call to Untappd //
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response.response)
-    })
+        var userNameResult = response.response.user.user_name
+        var beerNameResult = response.response.user.recent_brews.items[0].beer.beer_name
+        console.log(userNameResult);
+        console.log(beerNameResult);
+    });
+    }
+
+
+
 }

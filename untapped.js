ec889b556e283f45c15779bb129ca688adf47936
Stephen McMillen <Shtevetm@gmail.com>
2022-03-11 20:17:54 -0600

    Got the goddamn js DOM semi-functional. Submit button now appends the beer name on the page. Got a 429 error (too many requests) during testing. Changed page background too.

--- untapped.js
+++ untapped.js
@@ -1,8 +1,9 @@
 // UNTAPPED API Functions
 var clientID = "client_id=FC8119633681CAC495EBE7A545DFE9E44106539F&client_secret=191A6A679166A61D170CBF54E8F5D7CA3CAF0F42";
-
-
+var beerResult = ""
 // $(document).ready(userInfo())
+
+
 
 document.onload = userInfo();
 
@@ -27,36 +28,54 @@ function beerSearch() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response.response)
+        var beerResult = response.response.beers.items[0].beer;
+        console.log(beerResult)
+        console.log(result.result)
+        setTimeout(addElement(), 1000)
+        function addElement() {
+            // create a new div element
+            const newDiv = document.createElement("div");
+            newDiv.className = "beerResult"
+
+            // // and give it some content
+            const newContent = document.createTextNode(beerResult.beer_name);
+
+            // // add the text node to the newly created div
+            newDiv.appendChild(newContent);
+
+            // add the newly created element and its content into the DOM
+            const currentDiv = document.getElementById("div1");
+            document.body.insertBefore(newDiv, currentDiv);
+        }
     })
 }
+
 
 // USER INFO //
 function userInfo() {
     //Single username (For testing - so we don't hit API call limit)
-    var userName = ["Mr_JFlow"]
+    var userName = ["Quille"]
 
     //Full array of Usernames
     // var userName = ["Mr_JFlow", "Quille", "Shtevetm"]
-    
-    for (let i = 0; i<userName.length; i++){
-    var queryURL = "https://api.untappd.com/v4/user/info/" + userName[i] + "?" + clientID
 
-    // Ajax call to Untappd //
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var userNameResult = response.response.user.user_name
-        var beerNameResult = response.response.user.checkins.items[0].beer.beer_name
-        var stamp = response.response.user.checkins.items[0].created_at
-        console.log(userNameResult);
-        console.log(beerNameResult);
-        console.log(stamp)
-        console.log("~~~~~~~~~~~~")
-    });
+    for (let i = 0; i < userName.length; i++) {
+        var queryURL = "https://api.untappd.com/v4/user/info/" + userName[i] + "?" + clientID
+
+        // Ajax call to Untappd //
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var userNameResult = response.response.user.user_name
+            var beerNameResult = response.response.user.checkins.items[0].beer.beer_name
+            var stamp = response.response.user.checkins.items[0].created_at
+            console.log(userNameResult);
+            console.log(beerNameResult);
+            console.log(stamp)
+            console.log("~~~~~~~~~~~~")
+        });
     }
 
-
-
 }
+
